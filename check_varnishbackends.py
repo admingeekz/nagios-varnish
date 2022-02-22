080ff6e2a67bae6a224f14e88d88fd818316ed27
Dennis Ullrich <dennis.ullrich@plusserver.com>
2022-02-22 16:00:53 +0100

    Made it flake8 compliant

--- check_varnishbackends.py
+++ check_varnishbackends.py
@@ -33,59 +33,79 @@ import sys
 import optparse
 import subprocess
 
-exitCodes = [ 'OK', 'WARNING', 'CRITICAL', 'UNKNOWN']
 
 def runcommand(command, exit_on_fail=True):
     try:
-      process = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
-      output, unused_err = process.communicate()
-      retcode = process.poll()
-      return output
+        process = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
+        output, unused_err = process.communicate()
+        return output
 
     except OSError as e:
-      nexit(2, f"Error: Executing command failed, does it exist? {e}")
+        nexit(2, f"Error: Executing command failed: {e}")
+
 
 def nexit(exitCode, message):
-      print(f"{exitCodes[exitCode]}: {message}")
-      sys.exit(exitCode)
+    exitCodes = ('OK', 'WARNING', 'CRITICAL', 'UNKNOWN')
+    print(f"{exitCodes[exitCode]}: {message}")
+    sys.exit(exitCode)
+
 
 def main(argv):
-  o = optparse.OptionParser(conflict_handler="resolve", description="Nagios plugin to check varnish backend health.")
-  o.add_option('-H', '--host', action='store', type='string', dest='host', default='127.0.0.1', help='The ip varnishadm is listening on')
-  o.add_option('-P', '--port', action='store', type='int', dest='port', default=6082, help='The port varnishadm is listening on')
-  o.add_option('-s', '--secret', action='store', type='string', dest='secret', default='/etc/varnish/secret', help='The path to the secret file')
-  o.add_option('-p', '--path', action='store', type='string', dest='path', default='/usr/bin/varnishadm', help='The path to the varnishadm binary')
+    o = optparse.OptionParser(
+        conflict_handler="resolve",
+        description="Nagios plugin to check varnish backend health.")
+    o.add_option('-H', '--host',
+                 action='store', type='string', dest='host',
+                 default='127.0.0.1',
+                 help='The ip varnishadm is listening on')
+    o.add_option('-P', '--port',
+                 action='store', type='int', dest='port',
+                 default=6082,
+                 help='The port varnishadm is listening on')
+    o.add_option('-s', '--secret',
+                 action='store', type='string', dest='secret',
+                 default='/etc/varnish/secret',
+                 help='The path to the secret file')
+    o.add_option('-p', '--path',
+                 action='store', type='string', dest='path',
+                 default='/usr/bin/varnishadm',
+                 help='The path to the varnishadm binary')
 
-  options=o.parse_args()[0]
-  command = runcommand(f"{options.path} -S {options.secret} -T {options.host}:{options.port} backend.list")
-  backends = str(command.decode("utf-8")).split("\n")
-  backends_healthy, backends_sick = [], []
-  for line in backends:
-    if line.startswith("boot") and line.find("test")==-1:
-      if line.find("ealthy") != -1:
-        backends_healthy.append(line.split(" ")[0])
-      else:
-        backends_sick.append(line.split(" ")[0])
+    options = o.parse_args()[0]
+    command = runcommand(f"{options.path} -S {options.secret} "
+                         f"-T {options.host}:{options.port} backend.list")
+    backends = str(command.decode("utf-8")).split("\n")
+    backends_healthy, backends_sick = [], []
+    for line in backends:
+        if line.startswith("boot") and line.find("test") == -1:
+            if line.find("ealthy") != -1:
+                backends_healthy.append(line.split(" ")[0])
+            else:
+                backends_sick.append(line.split(" ")[0])
 
-  if backends_sick:
-    nexit(2, f"{len(backends_sick)}/{len(backends_healthy) + len(backends_sick)} backends are down: {', '.join(backends_sick)}")
+    if backends_sick:
+        totalBackends = len(backends_healthy) + len(backends_sick)
+        nexit(2, f"{len(backends_sick)}/{totalBackends} backends are down: "
+                 f"{', '.join(backends_sick)}")
 
-  if not backends_sick and not backends_healthy:
-    child_ok = False
-    command = runcommand(f"{options.path} -S {options.secret} -T {options.host}:{options.port} status")
-    response = str(command.decode("utf-8")).split("/n")
-    for line in response:
-      if line.startswith("Child in state running"):
-        child_ok = True
-        break
+    if not backends_sick and not backends_healthy:
+        child_ok = False
+        command = runcommand(f"{options.path} -S {options.secret} "
+                             f"-T {options.host}:{options.port} status")
+        response = str(command.decode("utf-8")).split("/n")
+        for line in response:
+            if line.startswith("Child in state running"):
+                child_ok = True
+                break
 
-    if child_ok:
-      nexit(0, "Child in state running")
+        if child_ok:
+            nexit(0, "Child in state running")
+        else:
+            nexit(1, "No backends detected. "
+                     "If this is an error, see readme.txt")
 
-    else:
-      nexit(1, "No backends detected. If this is an error, see readme.txt")
+    nexit(0, f"{len(backends_healthy)} backends are healthy.")
 
-  nexit(0, f"{len(backends_healthy)} backends are healthy.")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
